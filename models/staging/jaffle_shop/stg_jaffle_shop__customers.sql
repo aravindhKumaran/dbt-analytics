f9e772e5a26ec0b67fa397da2e263c45778054e8
Aravindh kumaran <aravindh1097@gmail.com>
2024-09-12 03:30:25 +0000

    added tests for sources and models

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,11 +1,10 @@
-WITH customers AS (
-    
-    SELECT  
-        id AS customer_id,
-        first_name,
-        last_name
-    
-    FROM {{ source('jaffle_shop', 'customers') }}
-)
+with
+    customers as (
 
-SELECT * FROM customers
+        select id as customer_id, first_name, last_name
+
+        from {{ source("jaffle_shop", "customers") }}
+    )
+
+select *
+from customers
